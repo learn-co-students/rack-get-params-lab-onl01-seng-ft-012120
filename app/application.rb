ffc9c2b1e433d6d4b945a9ac0a988bc91a78e791
Damon <dmarcr1997@gmail.com>
2020-02-24 16:43:05 -0600

    Done.

--- app/application.rb
+++ app/application.rb
@@ -45,7 +45,7 @@ class Application
       @@cart << item
       return "added #{item}"
     else
-      return "We don't have that item"
+      return "We don't have that item "
     end
   end
 end
